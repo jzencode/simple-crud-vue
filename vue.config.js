5b107cffaf5f2d416e2e8dc7bbbcd8099cc49fbc
Jayson Wong <jaysonwongf@gmail.com>
2021-10-04 18:37:53 +0800

    change target

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:5000/api',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         pathRewrite: { '^/api': '' },
       },
